67d811863bf6e0418944cac87fb89bacd1608818
AYUSH KUMAR <125579641+ayushmarch94@users.noreply.github.com>
2023-08-11 13:16:57 +0530

    created structure

---
+++ src/components/Main.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './Main.css';
+
+export default function Main() {
+  return (
+    <div>
+      <>
+        <div class="page">
+          <div class="a">hello</div>
+          <div class="a">hello</div>
+          <div class="a">hello</div>
+          <div class="a">hello</div>
+          <div class="a">hello</div>
+          <div class="a">hello</div>
+          <div class="a">hello</div>
+        </div>
+      </>
+    </div>
+  );
+}

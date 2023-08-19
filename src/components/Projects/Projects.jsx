5f8f7cab50f52e72e536d7a4293a4ab731fcfd03
AYUSH KUMAR <125579641+ayushmarch94@users.noreply.github.com>
2023-08-19 12:19:45 +0530

    Updated Projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -7,35 +7,35 @@ export default function Projects() {
         <div
         className='compdiv'>
           <div className='image'>
-            <img src={require("../../images/portfolio.png")} width={400} />
+            <img src={require("../../images/portfolio.png")} width={300} />
             <hr></hr>
           </div>
         </div>
         <div
         className='compdiv'>
           <div className='image'>
-            <img src={require("../../images/weather.png")} width={400} />
+            <img src={require("../../images/calculator.png")} width={300} />
             <hr></hr>
           </div>
         </div>
         <div
         className='compdiv'>
           <div className='image'>
-            <img src={require("../../images/calculator.png")} width={400} />
+            <img src={require("../../images/weather.png")} width={300} />
             <hr></hr>
           </div>
         </div>
         <div
         className='compdiv'>
           <div className='image'>
-            <img src={require("../../images/signup.png")} width={400} />
+            <img src={require("../../images/signup.png")} width={300} />
             <hr></hr>
           </div>
         </div>
         <div
         className='compdiv'>
           <div className='image'>
-            <img src={require("../../images/todo.png")} width={400} />
+            <img src={require("../../images/todo.png")} width={300} />
             <hr></hr>
           </div>
         </div>

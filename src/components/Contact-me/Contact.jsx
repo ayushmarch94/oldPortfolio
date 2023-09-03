210d5e6f76e183f1d9de85205e9b6b1cd122f06c
AYUSH KUMAR <125579641+ayushmarch94@users.noreply.github.com>
2023-09-03 23:42:53 +0530

    corrected error

--- src/components/Contact-me/Contact.jsx
+++ src/components/Contact-me/Contact.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import './Contact.css';
+import React from "react";
+import "./Contact.css";
 
 export default function Contact() {
-
   const redirect = () => {
-    window.open(
-      "https://www.linkedin.com/in/ayush-kumar-484013264/",
-      "_blank"
-    );
+    window.open("https://www.linkedin.com/in/ayush-kumar-484013264/", "_blank");
   };
-  
 
   return (
-    <div id="Contact" >
-      <img onClick={redirect} src={require('../../images/linkedin.png')} width={100} />
-      <p>Contact me</p>
-    </div>
-  )
+    <>
+      <div id="Contact">
+        <img
+          onClick={redirect}
+          src={require("../../images/linkedin.png")}
+          width={100}
+        />
+
+        <p>Contact me</p>
+      </div>
+    </>
+  );
 }
